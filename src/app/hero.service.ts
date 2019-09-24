d9e9e1b58650648fea48c795c1620eb362e8c2fe
Navneet Nagpal <navneetnagpal84@gmail.com>
2019-09-24 11:51:17 +0530

    http impl

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,17 +3,21 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heros';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService:MessageService) { }
+  private heroesUrl = 'data.json';  // URL to web api
+  constructor(  private http: HttpClient,
+    private messageService:MessageService) { }
 
   getHeroes():Observable<Hero[]> {
     this.messageService.add('HeroService:Fetching heroes');
     return of(HEROES);
+    // return this.http.get<Hero[]>(this.heroesUrl)
   }
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
